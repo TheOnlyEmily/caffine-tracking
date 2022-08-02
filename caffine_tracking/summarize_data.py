79ce528d490019b066bb0866bb86c723e3fff1cb
EmmyCodes <emingram49@gmail.com>
2022-08-02 15:23:59 -0600

    separate function not required

--- caffine_tracking/summarize_data.py
+++ caffine_tracking/summarize_data.py
@@ -3,5 +3,5 @@ from caff_data import get_caff_data
 
 if eq(__name__, "__main__"):
     caff_data = get_caff_data()
-    data_summary = get_data_summary(caff_data)
+    data_summary = caff_data.describe()
     print(data_summary)
