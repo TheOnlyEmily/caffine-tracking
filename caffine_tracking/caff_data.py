3b2d201162d541113d5e4a780c9b9ec1e93643aa
EmmyCodes <emingram49@gmail.com>
2022-08-03 11:19:29 -0600

    accounted for no csv file existing

--- caffine_tracking/caff_data.py
+++ caffine_tracking/caff_data.py
@@ -1,5 +1,21 @@
-import pandas as pd 
+import pandas as pd
+import os
+
+
+def get_new_caff_df():
+    return pd.DataFrame(columns=["Date", "Type", "Amount", "Units"])
+
+
+def get_current_caff_data():
+    return pd.read_csv("caff_data.csv", index_col=False, parse_dates=["Date"])
+
+
+def caff_data_csv_exists():
+    return os.path.exists("caff_data.csv")
 
 
 def get_caff_data():
-    return pd.read_csv("caff_data.csv", index_col=False, parse_dates=["Date"])
+    if caff_data_csv_exists():
+        return get_current_caff_data()
+    else:
+        return get_new_caff_df()
