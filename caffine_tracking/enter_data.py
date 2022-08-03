43d0c128d9041b48e63c48c1f1428f879f5a3091
EmmyCodes <emingram49@gmail.com>
2022-08-03 17:23:45 -0600

    get_command_line_args doesn't need its own file

--- caffine_tracking/enter_data.py
+++ caffine_tracking/enter_data.py
@@ -1,7 +1,11 @@
 from operator import eq
 from datetime import datetime
 from caff_data import validate_caff_units, append_to_data
-from comand_line_parse import get_command_line_args
+
+
+def get_command_line_args():
+    pass
+
 
 if eq(__name__, "__main__"):
     caff_type, caff_amount, caff_units = get_command_line_args()
