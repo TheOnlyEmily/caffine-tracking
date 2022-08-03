df50513b4bce3bb363263e88d2afa9c87119a8ec
EmmyCodes <emingram49@gmail.com>
2022-08-03 17:05:28 -0600

    prep for implementation of util functions

--- caffine_tracking/enter_data.py
+++ caffine_tracking/enter_data.py
@@ -1,7 +1,10 @@
 from operator import eq
+from datetime import datetime
+from caff_data import validate_caff_units, append_to_data
+from comand_line_parse import get_command_line_args
 
 if eq(__name__, "__main__"):
-    caff_type, caff_amount, caff_units = get_comand_line_args()
+    caff_type, caff_amount, caff_units = get_command_line_args()
     validate_caff_units(caff_units)
-    entry_time_stamp = get_entry_time_stamp()
+    entry_time_stamp = datetime.now()
     append_to_data(entry_time_stamp, caff_type, caff_amount, caff_units)
