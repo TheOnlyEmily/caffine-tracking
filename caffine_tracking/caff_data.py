cd9b24f752804032b8573681f985c43a9196756b
EmmyCodes <emingram49@gmail.com>
2022-08-03 16:19:00 -0600

    improved readability

--- caffine_tracking/caff_data.py
+++ caffine_tracking/caff_data.py
@@ -2,19 +2,19 @@ import pandas as pd
 import os
 
 
-def get_new_caff_df():
+def get_new_caff_df() -> pd.DataFrame:
     return pd.DataFrame(columns=["Date", "Type", "Amount", "Units"])
 
 
-def get_current_caff_data():
+def get_current_caff_data() -> pd.DataFrame:
     return pd.read_csv("caff_data.csv", index_col=False, parse_dates=["Date"])
 
 
-def caff_data_csv_exists():
+def caff_data_csv_exists() -> bool:
     return os.path.exists("caff_data.csv")
 
 
-def get_caff_data():
+def get_caff_data() -> pd.DataFrame:
     if caff_data_csv_exists():
         return get_current_caff_data()
     else:
