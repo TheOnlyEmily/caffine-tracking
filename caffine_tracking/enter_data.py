830d9a5b465f9ee7e29577f9117467df45608978
EmmyCodes <emingram49@gmail.com>
2022-08-13 16:06:21 -0600

    block out utility functions

--- caffine_tracking/enter_data.py
+++ caffine_tracking/enter_data.py
@@ -1,9 +1,32 @@
 from operator import eq
 from datetime import datetime
-from caff_data import validate_caff_units, append_to_data
+from caff_data import append_to_data
 
 
-def get_command_line_args():
+def validate_caff_units(caff_unit: str) -> bool:
+    """
+    Determines if units have the correct dimensionality (volume in this case).
+
+    Args:
+        caff_unit: A string respresentation of a unit of measurement.
+
+    Returns:
+        A boolean value, where True means that caff_unit is volumetric,
+        and False if it's not.
+    """
+    pass
+
+
+def get_command_line_args() -> tuple[str, int, str]:
+    """
+    Extracts three arguments from the command line: the caffine source, 
+    the amount of the source, and what units the source amount is measured in, 
+    respectively. 
+
+    Returns:
+        A tuple containing the caffine source name, the amount of the source,
+        and the units for measuring the caffine amount. 
+    """
     pass
 
 
